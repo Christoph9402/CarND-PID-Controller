7d029b408ab9d3222174ec74e801272940102f6d
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-09-29 22:29:09 +0200

    Changed PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,23 +9,24 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+   Kp_ = Kp;
+   Ki_= Ki;
+   Kd_=Kd;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+    cte_d=cte-cte_p;
+    cte_p=cte;
+    cte_i=cte_i + cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  double alpha = -tau_p * cte - tau_d * diff_cte - tau_i * it_cte;
+  double alpha = -tau_p * cte_p - tau_d * cte_d - tau_i * it_cte;
   return alpha;  // TODO: Add your total error calc here!
 }
