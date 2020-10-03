3eec9106b3400c26617f7169667d32aaa57c7035
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-10-03 22:03:01 +0200

    type: style
    Deleting unused comments and code segments from previous versions. Also I added new comments for a better understanding of the calculations and referring to the udacity lesson to be able to get a deeper understanding if necessary

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,8 +1,5 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
 
 PID::PID() {}
 
@@ -14,31 +11,23 @@ void PID::Init(double Kp, double Ki, double Kd) {
    this->Kd=Kd;
 
    //Initializing errors and previous_cte with 0.0
-   //previous_cte=0;
    p_error=0;
    i_error=0;
    d_error=0;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-    // D-component:
-    d_error=cte-p_error;
-   //P-component:
-    p_error=cte;
-    // I-component:
+
+    // D-component: calculated by subtracting p_error (which is 0 in the first step) from the cte
+    d_error = cte - p_error;
+    //P-component:set the p_error to the current cte
+    p_error = cte;
+    // I-component: add the current cte to the previous i_error
     i_error += cte;
-
-    //previous_cte=cte;
-
-
-    //std::cout << "CTE: " << cte << " error_d: " << d_error << " error_p: " << p_error << " error_i: " << i_error << std::endl;
 }
 
 double PID::TotalError() {
+    //Calculate and return the total error using the formula presented in the lesson 12.12
   double error_t = -Kp * p_error - Kd * d_error - Ki * i_error;
   return error_t;
-  // TODO: Add your total error calc here!
 }
